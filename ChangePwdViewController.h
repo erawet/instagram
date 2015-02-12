2f5a10fbae6294145e94aa5a7cdde3043c3c5cd5
Don Wettasinghe <erawet@gmail.com>
2015-02-12 12:07:59 -0500

    finish settings UI and change password view

---
+++ ChangePwdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePwdViewController.h
+//  instagram
+//
+//  Created by Don Wettasinghe on 2/11/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangePwdViewController : UIViewController
+
+@end
---
+++ SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  instagram
+//
+//  Created by Don Wettasinghe on 2/11/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
